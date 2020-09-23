ad34d9009aefd7aa0fe5a32daee92f552daa94cd
Jianing <jianingw321@outlook.com>
2020-09-23 14:30:56 +0800

    Updates

---
+++ precache-manifest.b39ac6d43247093ff54019d980bfb5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db1e2b0744931da3b5e24490277964c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "61ce59e4ce0e5a189768",
+    "url": "./static/css/main.d1daebdd.chunk.css"
+  },
+  {
+    "revision": "e9de09beb5257964fadb",
+    "url": "./static/js/2.e2e8114c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e2e8114c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61ce59e4ce0e5a189768",
+    "url": "./static/js/main.2402d352.chunk.js"
+  },
+  {
+    "revision": "2854f216fbdbb5eb459d",
+    "url": "./static/js/runtime-main.32f9304b.js"
+  }
+]);
